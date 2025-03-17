e9e494f8f2ee7611522c19fd61263d14e0333c91
Abdhulla1 <abdhulla.code@gmail.com>
2025-03-17 23:27:29 +0530

    updated17v3

--- src/components/dashboardComponents/AdminBoard/AdminStatusCard.js
+++ src/components/dashboardComponents/AdminBoard/AdminStatusCard.js
@@ -7,9 +7,9 @@ const AdminStatusCard = ({ title, statuses }) => {
       <div className="flex flex-col md:flex-row flex-wrap justify-between mb-2 text-gray-700">
         {statuses.map((status, index) => (
           <div key={index} className="flex items-center mb-2 text-gray-700 w-full md:w-auto">
-            <div className="flex flex-1 flex-col items-center md:items-start">
-              <div className="flex flex-1 items-center">
-                <span className="bg-black text-white rounded-md p-1 w-full text-center flex justify-center items-center mr-2">
+            <div className="flex  flex-col items-center md:items-start">
+              <div className="flex  items-center">
+                <span className="bg-black text-white rounded-md p-1  text-center flex justify-center items-center mr-2">
                   {status.count}
                 </span>
                 <span className="text-center md:text-left break-words">{status.label}</span>
